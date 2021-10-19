b440f46a98eada2ed9c9ef454e4452d702d8af5b
Assyl00 <a.d.022106@gmail.com>
2021-10-20 01:36:16 +0600

    refactor: checkbox fixed

--- todo/src/Task/TaskList/TaskList.jsx
+++ todo/src/Task/TaskList/TaskList.jsx
@@ -1,19 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./TaskList.css";
+import { useState } from "react";
 
 export default function TaskList({ todo, removeTodo }) {
+  const [className, setClassName] = useState("task-list-title");
   return (
     <>
       <div className="task-list-checked">
-        <label>
+        <label className="container">
           <input
             type="checkbox"
             value="todo"
             onChange={() => {
               todo.checked = !todo.checked;
+              console.log(todo.checked);
+              if(todo.checked === true){
+                // className += ' active';
+                setClassName('task-list-title active')
+              }
+              else setClassName('task-list-title');
+              console.log(className);
             }}
+            // className={className}
           />
-          <h4 className="task-list-title">{todo.todoText}</h4>
+          <h4 className={className}>{todo.todoText}</h4>
         </label>
       </div>
       <p className="task-list-date">{todo.date}</p>
@@ -28,8 +39,3 @@ export default function TaskList({ todo, removeTodo }) {
     </>
   );
 }
-
-// TaskList.PropTypes = {
-//   list: PropTypes.arrayOf(PropTypes.object).isRequired
-// };
-// //
