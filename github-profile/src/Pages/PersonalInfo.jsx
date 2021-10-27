27e7043c52442654806123ff5bb63fdf654a7de1
Assan21 <65251749+Luigi210@users.noreply.github.com>
2021-10-27 20:55:54 +0600

    Repository

--- github-profile/src/Pages/PersonalInfo.jsx
+++ github-profile/src/Pages/PersonalInfo.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {useLocation} from 'react-router-dom';
+import {useLocation, Link} from 'react-router-dom';
+
 
 export default function PersonalInfo(){
     const location = useLocation();
@@ -9,7 +10,18 @@ export default function PersonalInfo(){
             <h1>
                 Personal Info
             </h1>
-            <p>Name: {item.name}</p>
+            <h3>Name: {item.name}</h3>
+            <h4>Login: {item.login}</h4>
+            <h5>Github Url: <a href={item.html_url}>{item.name}</a></h5>
+            <p>Bio: {item.bio}</p>
+            <p>Company: {item.company}</p>
+            <p>Followers: {item.followers}</p>
+            <p>Following: {item.following}</p>
+            <p>Organization: <a href={item.organizations_url} target={"_blank"}>{item.organizations_url}</a></p>
+            <Link to={{
+                pathname: '/repo/' + item.name,
+                state: {item}
+            }}>Ustudy</Link>
         </>
     );
 }
--- github-profile/src/Pages/Repository.jsx
+++ github-profile/src/Pages/Repository.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
-
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import {useLocation} from 'react-router-dom';
 
 export default function Repository(){
+    const location = useLocation();
+    const { item } = location.state;
+    const [repos, setRepos] = useState([]);
+    useEffect(async () => {
+        const apiUrl = `https://api.github.com/users/${item.login}/repos`;
+        const res = await axios.get(apiUrl);
+        setRepos([...repos, res.data]);
+        console.log(repos);
+    }, [])
+
     return(
         <>
             <h2>Ustudy</h2>
+            <p>{item.name}</p>
+            <a href={item.html_url}>Link</a>
+            <ul>
+                {repos.map((repo, index) => {
+                    <li>{repo.id}</li>
+                })}
+            </ul>
         </>
     );
 }
