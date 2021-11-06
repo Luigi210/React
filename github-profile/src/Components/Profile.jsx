668c277c1715cd4e682a9d16665a251f1af83f71
Assan21 <65251749+Luigi210@users.noreply.github.com>
2021-11-06 17:49:13 +0600

    refactor: again delete realization

--- github-profile/src/Components/Profile.jsx
+++ github-profile/src/Components/Profile.jsx
@@ -1,81 +1,109 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
-import {Link} from 'react-router-dom';
-import './Profile.sass';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import "./Profile.sass";
 import PersonalInfo from "../Pages/PersonalInfo";
 // import {Icon} from "@material-ui/core";
-import {Icon} from "semantic-ui-react";
+import { Icon } from "semantic-ui-react";
 
-function Profile(){
-    const [user, setUser] = useState('')
-    const [profile, setProfile] = useState([]);
-    const [repos, setRepos] = useState([]);
+function Profile() {
+  const [user, setUser] = useState("");
+  const [profile, setProfile] = useState([]);
+  const [repos, setRepos] = useState([]);
 
-    useEffect(() => {
-        try {
-            const arrProfiles = localStorage.getItem("profiles");
-            const parsedArray = JSON.parse(arrProfiles);
-            console.log(parsedArray);
-            setProfile(parsedArray);
-        }
-        catch (e) {
-            console.log(e);
-        }
-    }, [])
-
-    useEffect(() => {
-        localStorage.setItem("profiles", JSON.stringify(profile));
-        console.log(profile);
-        return () => {
-            console.log("123");
-        }
-    }, [profile])
-
-    async function getProfile(){
-        if (profile.findIndex(profileItem => profileItem.login.toLowerCase() === user.toLowerCase()) !== -1) {
-            return;
-        }
-
-        const apiUrl = `https://api.github.com/users/${user}`;
-        const res = await axios.get(apiUrl);
-
-        setProfile([...profile, res.data]);
-        setUser('');
+  useEffect(() => {
+    try {
+      const arrProfiles = localStorage.getItem("profiles");
+      const parsedArray = JSON.parse(arrProfiles);
+      console.log(parsedArray);
+      setProfile(parsedArray);
+    } catch (e) {
+      console.log(e);
     }
-    return (
-        <div>
-            <div>
-                <input
-                    className={"search-profile"}
-                    value={user}
-                    type={'text'}
-                    onChange={event => {setUser(event.target.value)}}
-                    placeholder="Enter the name of user"
-                    />
-                <button className={"add-button"} onClick={getProfile}>Add</button>
-            </div>
-            <div className="d-flex flex-wrap">
-                {profile.map((item, index) => {
-                    return (
-                        <React.Fragment key={index}>
-                            <div className="profile-block">
-                                <div className={"profile-title"}>
-                                    <h3 className={"profile-name"}>{item.name}</h3>
-                                    {/*<Icon name={"download"}/>*/}
-                                    {/*<Icon name={"trash alternate outline"} link size={"huge"}/>*/}
-                                </div>
-                                <img src={item.avatar_url} className="img-thumbnail"/>
-                                <Link to={{
-                                    pathname: '/info/' + item.name,
-                                    state: {item}
-                                }}  className="btn btn-primary w-100" >Open Profile</Link>
-                            </div>
-                        </React.Fragment>
-                    )
-                })}
-            </div>
-        </div>
-    );
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("profiles", JSON.stringify(profile));
+    console.log(profile);
+    return () => {
+      console.log("123");
+    };
+  }, [profile]);
+
+  async function getProfile() {
+    if (
+      profile.findIndex(
+        (profileItem) => profileItem.login.toLowerCase() === user.toLowerCase()
+      ) !== -1
+    ) {
+      return;
+    }
+
+    const apiUrl = `https://api.github.com/users/${user}`;
+    const res = await axios.get(apiUrl);
+
+    setProfile([...profile, res.data]);
+    setUser("");
+  }
+
+  function deleteProfile(index) {
+    setProfile(profile.filter((element, ind) => ind !== index));
+    // console.log(profile);
+    // setProfile(profile);
+  }
+
+  return (
+    <div>
+      <div>
+        <input
+          className={"search-profile"}
+          value={user}
+          type={"text"}
+          onChange={(event) => {
+            setUser(event.target.value);
+          }}
+          placeholder="Enter the name of user"
+        />
+        <button className={"add-button"} onClick={getProfile}>
+          Add
+        </button>
+      </div>
+      <div className="d-flex flex-wrap">
+        {profile.map((item, index) => {
+          return (
+            <React.Fragment key={index}>
+              <div className="profile-block">
+                <div className={"profile-title"}>
+                  <h3 className={"profile-name"}>{item.name}</h3>
+                  {/*<Icon name={"download"}/>*/}
+                  {/*<Icon name={"trash alternate outline"} link size={"huge"}/>*/}
+                </div>
+                <img src={item.avatar_url} className="img-thumbnail" />
+                <Link
+                  to={{
+                    pathname: "/info/" + item.name,
+                    state: { item },
+                  }}
+                  className="btn btn-primary w-100"
+                >
+                  Open Profile
+                </Link>
+                <button
+                  className="btn btn-secondary w-100"
+                  onClick={() => {
+                    return deleteProfile(index);
+                    // console.log("us");
+                  }}
+                >
+                  Delete
+                </button>
+              </div>
+            </React.Fragment>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Profile;
--- github-profile/src/Pages/Repository.jsx
+++ github-profile/src/Pages/Repository.jsx
@@ -1,70 +1,73 @@
-import React, {useState, useEffect, useRef} from 'react';
-import axios from 'axios';
-import {useLocation} from 'react-router-dom';
-import './Repository.sass';
+import React, { useState, useEffect, useRef } from "react";
+import axios from "axios";
+import { useLocation } from "react-router-dom";
+import "./Repository.sass";
 
-export default function Repository(){
-    const location = useLocation();
-    const { item } = location.state;
-    const [repos, setRepos] = useState([]);
-    const reference = useRef([]);
-    useEffect( () => {
-        getRepos();
-        console.log(repos.length);
-    }, [])
+export default function Repository() {
+  const location = useLocation();
+  const { item } = location.state;
+  const [repos, setRepos] = useState([]);
+  const reference = useRef([]);
+  useEffect(() => {
+    getRepos();
+    console.log(repos.length);
+  }, []);
 
-    async function getRepos() {
-        const apiUrl = `https://api.github.com/users/${item.login}/repos`;
-        const res = await axios.get(apiUrl);
-        // setRepos([...repos, res.data]);
-        if (repos.length === 0) {
-            setRepos([...repos, res.data]);
-            console.log(repos.length);
-        }
-        console.log(repos[0]);
+  async function getRepos() {
+    const apiUrl = `https://api.github.com/users/${item.login}/repos`;
+    const res = await axios.get(apiUrl);
+    // setRepos([...repos, res.data]);
+    if (repos.length === 0) {
+      setRepos([...repos, res.data]);
+      console.log(repos.length);
     }
+    console.log(repos[0]);
+  }
 
-    const showInfo = (index) => {
-        const node = reference.current[index];
-        console.log(node);
-        if (node.getAttribute('class').indexOf('hidden-info') !== -1){
-            node.setAttribute('class', 'open-info');
-        }
-        else if (node.getAttribute('class').indexOf('hidden-info') === -1){
-            node.setAttribute('class', 'hidden-info');
-        }
+  const showInfo = (index) => {
+    const node = reference.current[index];
+    console.log(node);
+    if (node.getAttribute("class").indexOf("hidden-info") !== -1) {
+      node.setAttribute("class", "open-info");
+    } else if (node.getAttribute("class").indexOf("hidden-info") === -1) {
+      node.setAttribute("class", "hidden-info");
     }
+  };
 
-    return(
-        <>
-            <h2>Ustudy</h2>
-            <p>{item.name}</p>
-            <a href={item.html_url}>Link</a>
-            <div>
-                <p>Repositories</p>
-                <ul>
-                    { repos.length === 1 && repos[0].map((it, index) =>
-                        <>
-                            <li
-                                className={"repo-item"}
-                                key={index}
-                                onClick={() => {
-                                    showInfo(index);
-                                    console.log(repos[0]);
-                                }}
-
-                            >{it.name}</li>
-                            <div key={index}
-                               className={"hidden-info"}
-                               ref={(element) => reference.current.push(element)}
-                            >
-                                <p>ID: {it.id}</p><br></br>
-                                <a href={it.url}>Link</a>
-                            </div>
-                        </>)
-                    }
-                </ul>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <h2>Ustudy</h2>
+      <p>{item.name}</p>
+      <a href={item.html_url}>Link</a>
+      <div>
+        <p>Repositories</p>
+        <ul>
+          {repos.length === 1 &&
+            repos[0].map((it, index) => (
+              <>
+                <li
+                  className={"repo-item"}
+                  key={index}
+                  onClick={() => {
+                    showInfo(index);
+                    console.log(repos[0]);
+                  }}
+                >
+                  {it.name}
+                </li>
+                <div
+                  key={index}
+                  className={"hidden-info"}
+                  ref={(element) => reference.current.push(element)}
+                >
+                  <p>ID: {it.id}</p>
+                  <br></br>
+                  <a href={it.url}>Link</a>
+                </div>
+              </>
+            ))}
+        </ul>
+      </div>
+    </>
+  );
 }
